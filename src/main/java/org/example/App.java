cc5d0018f96605422472f826c9c4af0779327109
pikahua <827839635@qq.com>
2022-05-23 00:54:00 +0800

    course

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -10,4 +10,24 @@ public class App
     {
         System.out.println( "Hello World!" );
     }
+
+    public static void merge(int[] nums1, int m, int[] nums2, int n) {
+        int cur = m + n - 1;
+        while (n > 0) {
+            if (m == 0) {
+                while (n > 0) {
+                    nums1[cur--] = nums2[n - 1];
+                    n--;
+                }
+            } else {
+                if (nums1[m - 1] <= nums2[n - 1]) {
+                    nums1[cur--] = nums2[n - 1];
+                    n--;
+                } else {
+                    nums1[cur--] = nums1[m - 1];
+                    m--;
+                }
+            }
+        }
+    }
 }
