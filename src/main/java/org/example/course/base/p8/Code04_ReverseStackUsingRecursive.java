f19687e5a0d64a3d8ffa781e6130304dfe4dbcd5
pikahua <827839635@qq.com>
2022-09-30 02:10:33 +0800

    add annotation

--- src/main/java/org/example/course/base/p8/Code04_ReverseStackUsingRecursive.java
+++ src/main/java/org/example/course/base/p8/Code04_ReverseStackUsingRecursive.java
@@ -2,6 +2,7 @@ package org.example.course.base.p8;
 
 import java.util.Stack;
 
+// 给你一个栈，请你逆序这个栈，不能申请额外的数据结构，只能使用递归函数。
 public class Code04_ReverseStackUsingRecursive {
 
 	public static void reverse(Stack<Integer> stack) {
--- src/main/java/org/example/course/base/p8/Code08_CardsInLine.java
+++ src/main/java/org/example/course/base/p8/Code08_CardsInLine.java
@@ -1,5 +1,8 @@
 package org.example.course.base.p8;
 
+// 给定一个整型数组arr，代表数值不同的纸牌排成一条线。玩家A和玩家B依次拿走每张纸
+//牌，规定玩家A先拿，玩家B后拿，但是每个玩家每次只能拿走最左或最右的纸牌，玩家A
+//和玩家B都绝顶聪明。请返回最后获胜者的分数。
 public class Code08_CardsInLine {
 
 	public static int win1(int[] arr) {
