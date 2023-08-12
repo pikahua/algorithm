6d61cd5593c2a1dcce991087893edab585994a8b
pikahua <827839635@qq.com>
2023-08-12 18:41:08 +0800

    format code

--- src/main/java/org/example/leetcode/MergeTwoLists.java
+++ src/main/java/org/example/leetcode/MergeTwoLists.java
@@ -1,23 +1,33 @@
 package org.example.leetcode;
 
- class ListNode {
-     int val;
-     ListNode next;
-     ListNode() {}
-     ListNode(int val) { this.val = val; }
-     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- }
-
 public class MergeTwoLists {
+
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
         ListNode head = new ListNode();
         ListNode cur = head;
-        while (list1 != null && list2 != null){
+        while (list1 != null && list2 != null) {
             ListNode listNode = new ListNode();
-            if(list1.val <= list2.val){
+            if (list1.val <= list2.val) {
                 listNode.val = list1.val;
                 list1 = list1.next;
-            }else{
+            } else {
                 listNode.val = list2.val;
                 list2 = list2.next;
             }
